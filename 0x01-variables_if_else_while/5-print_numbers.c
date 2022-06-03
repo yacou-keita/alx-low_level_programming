c8bb8e6151c2cbeff4caa4b027652d63ea0cafc9
Yacouba Keita <yacoukeita0212@gmail.com>
2022-06-03 10:07:21 -0700

    adding 5-print_numbers.c file

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+/**
+ * main - prints numbers from 0 to 9
+ *
+ * Return: Always (Success)
+ *
+ */
+int main(void)
+{
+	
+	int i;
+
+	for (i = 0; i < 10; i++)
+	{
+		printf("%d", i);
+	}
+	putchar('\n');
+	return (0);
+}
