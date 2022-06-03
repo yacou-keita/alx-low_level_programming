7f8749ea9c260699602f0b8119b17a3f84b10ad4
Yacouba Keita <yacoukeita0212@gmail.com>
2022-06-03 09:42:57 -0700

    adding 2-print_alphabet.c file

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+/**
+ * main - prinf the alphabet in lowercase
+ *
+ * Return: Always (Success)
+ *
+ */
+int main(void)
+{
+	char alp[26] = "abcdefghijklmnopqrstuvwxyz";
+	int i;
+
+	for (i = 0; i < 26; i++)
+	{
+		putchar(alp[i]);
+	}
+	putchar('\n');
+	return (0);
+}
