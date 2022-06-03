637cde98b80a03fd71971bf89887a2f0d605ab33
Yacouba Keita <yacoukeita0212@gmail.com>
2022-06-02 17:05:14 -0700

    update 4-puts.c file

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,10 +1,12 @@
 #include <stdio.h>
+
 /**
- * main - main block
- * return: 0
+ * main - Entry point
+ *
+ * Return: 0
  */
-int main(void) 
+int main(void)
 {
-	puts("\"Programming is like building a multilingual puzzle");
-	return (0);
+    puts("\"Programming is like building a multilingual puzzle");
+    return (0);
 }
