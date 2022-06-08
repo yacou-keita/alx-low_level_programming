354bafd6bd8e7592e604cab253a5ae15927c12ad
Yacouba Keita <yacoukeita0212@gmail.com>
2022-06-07 18:17:04 -0700

    adding 103-fibonacci.c

--- 0x02-functions_nested_loops/103-fibonacci.c
+++ 0x02-functions_nested_loops/103-fibonacci.c
@@ -1,25 +1,28 @@
 #include <stdio.h>
 
 /**
- * main - Prints the add of the Fibonacci numbers
+ * main - Prints the add of the even-valued
+ * fibonacci numbers.
  *
  * Return: Always 0.
  */
 int main(void)
 {
-	int c;
-	long int n1, n2, fn;
+	long int n1, n2, fn, afn;
 
 	n1 = 1;
 	n2 = 2;
-	printf("%ld, %ld", n1, n2);
-	for (c = 0; c < 48; c++)
+	fn = afn = 0;
+	while (fn <= 4000000)
 	{
 		fn = n1 + n2;
-		printf(", %ld", fn);
 		n1 = n2;
 		n2 = fn;
+		if ((n1 % 2) == 0)
+		{
+			afn += n1;
+		}
 	}
-	printf("\n");
+	printf("%ld\n", afn);
 	return (0);
 }
